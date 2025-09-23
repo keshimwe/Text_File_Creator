c5188326a717897f3528528d3e825bcab881a474
Keshi <keshi.mwe@gmail.com>
2025-09-23 19:55:36 -0400

    first commit

---
+++ file_creator.py
@@ -0,0 +1,7 @@
+name = input("What's your name? \n")
+color = input("What's your favorite color? /n")
+with open('example.txt', 'w') as file:
+    file.write(f"{name} created this file")
+
+    with open('example.txt', 'w') as file:
+        file.write(f"{color} created this file")
